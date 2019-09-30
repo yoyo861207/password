7c6d7fa071667febc0f370a640671af6f814fcf7
karen lin <yoyo861207@gmail.com>
2019-09-30 15:00:24 +0800

    advanced version

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a12'
 i = 3 #剩餘機會
+i = i - 1
 while i > 0 :
 	pwd = input('please enter the password')
 	if pwd == password :
 		print('log in  successfully')
 		break # 逃出迴圈
 	else : 
-		i = i - 1
-		print('pwd error! you still have ',i,'opportunities') 
+		print('pwd is wrong')
+		if i > 0 :
+			print('you still have ',i,'opportunities')
+		else :
+			print('no opportunity , your account is blocked')
